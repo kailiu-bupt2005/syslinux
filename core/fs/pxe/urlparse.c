449e54c75f0f7d1323b9eecb9ab1002e7e87cf6c
H. Peter Anvin <hpa@zytor.com>
2009-10-06 14:30:57 -0700

    core: pxe: urlparse: remove definitions from header file
    Remove definitions we already have in the header file.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- core/fs/pxe/urlparse.c
+++ core/fs/pxe/urlparse.c
@@ -9,22 +9,6 @@
 #include <stdio.h>
 #include "url.h"
 
-enum url_type {
-    URL_NORMAL,
-    URL_OLD_TFTP,
-    URL_PREFIX
-};
-
-struct url_info {
-    char *scheme;
-    char *user;
-    char *passwd;
-    char *host;
-    unsigned int port;
-    char *path;			/* Includes query */
-    enum url_type type;
-};
-
 void parse_url(struct url_info *ui, char *url)
 {
     char *p = url;
